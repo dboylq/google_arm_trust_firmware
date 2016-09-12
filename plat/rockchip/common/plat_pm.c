c7b221b7968fd3c7b2787dbc2e6808d32ebe69b1
Julius Werner <jwerner@chromium.org>
2016-09-12 12:24:34 -0700

    Revert "rockchip: fix the scu idle for rk3399"
    This reverts commit c467d412da76404119ef1b1defb2343f50cb2c3b.
    
    This seems to pretty clearly break suspend/resume. Without it, suspend
    still works only very inconsistently on my kernel, but with it it
    doesn't work at all.
    
    BRANCH=gru
    BUG=chrome-os-partner:57336
    TEST=suspend/resume seems notably more stable (read: more than 0%
    stable) with this revert.
    
    Change-Id: I2a02962a01343abd0cba47ed63192c1cdf88b173
    Signed-off-by: Julius Werner <jwerner@chromium.org>

--- plat/rockchip/common/plat_pm.c
+++ plat/rockchip/common/plat_pm.c
@@ -195,9 +195,6 @@ void rockchip_pwr_domain_suspend(const psci_power_state_t *target_state)
 	if (RK_CLUSTER_PWR_STATE(target_state) == PLAT_MAX_OFF_STATE)
 		plat_cci_disable();
 
-	if (RK_SYSTEM_PWR_STATE(target_state) == PLAT_MAX_OFF_STATE)
-		return;
-
 	if (!rockchip_ops || !rockchip_ops->hlvl_pwr_dm_suspend)
 		return;
 
@@ -266,12 +263,6 @@ void rockchip_pwr_domain_suspend_finish(const psci_power_state_t *target_state)
 	if (!rockchip_ops)
 		goto comm_finish;
 
-	if (RK_SYSTEM_PWR_STATE(target_state) == PLAT_MAX_OFF_STATE) {
-		if (rockchip_ops->sys_pwr_dm_resume)
-			rockchip_ops->sys_pwr_dm_resume();
-		goto comm_finish;
-	}
-
 	if (rockchip_ops->hlvl_pwr_dm_resume) {
 		for (lvl = MPIDR_AFFLVL1; lvl <= PLAT_MAX_PWR_LVL; lvl++) {
 			lvl_state = target_state->pwr_domain_state[lvl];
@@ -279,16 +270,20 @@ void rockchip_pwr_domain_suspend_finish(const psci_power_state_t *target_state)
 		}
 	}
 
-	if (rockchip_ops->cores_pwr_dm_resume)
+	if (RK_SYSTEM_PWR_STATE(target_state) == PLAT_MAX_OFF_STATE &&
+	    rockchip_ops->sys_pwr_dm_resume) {
+		rockchip_ops->sys_pwr_dm_resume();
+	} else if (rockchip_ops->cores_pwr_dm_resume) {
 		rockchip_ops->cores_pwr_dm_resume();
-	/*
-	 * Program the gic per-cpu distributor or re-distributor interface.
-	 * For sys power domain operation, resuming of the gic needs to operate in
-	 * rockchip_ops->sys_pwr_dm_resume, according to the sys power mode implements.
-	*/
-	plat_rockchip_gic_cpuif_enable();
+	}
 
 comm_finish:
+	/*
+	 * Program the gic per-cpu distributor
+	 * or re-distributor interface
+	 */
+	plat_rockchip_gic_cpuif_enable();
+
 	/* Perform the common cluster specific operations */
 	if (RK_CLUSTER_PWR_STATE(target_state) == PLAT_MAX_OFF_STATE) {
 		/* Enable coherency if this cluster was off */
